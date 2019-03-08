72ac591114078e8e4a706b3879ea7b615ec5730f
Rachel Chen <rachelchen0418@gmail.com>
2019-03-07 22:57:48 -0500

    Final project of birdie:) Using Twitter API

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -1,39 +1,116 @@
 //
 //  HomeTableViewController.swift
-//  
+//  Twitter
 //
-//  Created by Rachel Chen on 3/1/19.
+//  Created by Rachel Chen on 3/7/19.
+//  Copyright © 2019 Dan. All rights reserved.
 //
 
 import UIKit
 
 class HomeTableViewController: UITableViewController {
+    
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
+    
+    let myRefreshControl = UIRefreshControl()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadTweets()
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
     }
 
+    
+    @objc func loadTweets(){
+        
+        
+        numberOfTweet = 20
+        
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        
+        let myParms = ["count": numberOfTweet]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParms, success:
+            { (tweets: [NSDictionary]) in
+                
+                self.tweetArray.removeAll()
+                
+                for tweet in tweets {
+                    self.tweetArray.append(tweet)
+                }
+                
+                self.tableView.reloadData()
+                self.myRefreshControl.endRefreshing()
+                
+        }, failure: {(Error) in
+            print("Couldn't retreive tweets. Oh my!")
+        })
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
+        if indexPath.row + 1 == tweetArray.count {
+            loadMoreTweets()
+        }
+    }
+    
+    func loadMoreTweets(){
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        
+        numberOfTweet = numberOfTweet + 20
+        let myParms = ["count": numberOfTweet]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParms, success:
+            { (tweets: [NSDictionary]) in
+                
+                self.tweetArray.removeAll()
+                
+                for tweet in tweets {
+                    self.tweetArray.append(tweet)
+                }
+                
+                self.tableView.reloadData()
+                
+        }, failure: {(Error) in
+            print("Couldn't retreive tweets. Oh my!")
+        })
+        
+    }
+    
+    
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    }
     // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        
+        let user =  tweetArray[indexPath.row]["user"] as! NSDictionary
+        
+        cell.userNameLabel.text = user["name"] as? String
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
+        
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageUrl!)
+        
+        if let imageData = data {
+            cell.profileImageView.image = UIImage(data: imageData)
+        }
+        
+        return cell
+    }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        return tweetArray.count
     }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = UITableViewCell()
-            return cell
-        }
-    }
-
+}
--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -20,10 +20,18 @@ class LoginViewController: UIViewController {
         loginButton.layer.cornerRadius = 15
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
 
     @IBAction func onTapLogin(_ sender: Any) {
         let urlString = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: urlString, success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            
             // on login success, present homeTableVC
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
